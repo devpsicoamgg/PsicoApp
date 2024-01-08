f886c3d08f2db914f73325a1b6af6255b8f22ec3
Alberto Mario Guerrero G <psico.amgg@gmail.com>
2024-01-07 22:38:20 -0500

    HomePage

--- client/src/components/homePage/homePage.jsx
+++ client/src/components/homePage/homePage.jsx
@@ -1,4 +1,3 @@
-// components/HomePage.js
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import ROUTES from '../../routes/routesHelper';
@@ -8,43 +7,44 @@ import MONITORING from '../../assets/img/seguimiento.png';
 import INFORMS from '../../assets/img/informes.png';
 import EDITION from '../../assets/img/edicion.png';
 import SETTINGS from '../../assets/img/ajustes.png';
+import styles from './homePage.module.css'; 
 
 function HomePage() {
   return (
-    <div className="parent">
-      <div className="div1">
-        <NavLink to={ROUTES.BASIC_DATA_FORM} className="btn-basic-data-form">
-          <img src={APERTURA} alt="BATOS BASICOS" className="img-basic-data-form" />
+    <div className={styles.parent}>
+      <div className={styles.div1}>
+        <NavLink to={ROUTES.BASIC_DATA_FORM} className={styles['btn-basic-data-form']}>
+          <img src={APERTURA} alt="BATOS BASICOS" className={styles['img-basic-data-form']} />
         </NavLink>
       </div>
 
-      <div className="div2">
-        <Link to={ROUTES.CLINICALHISTORY} className="btn-clinical-history">
-          <img src={CLINICALHISTORY} alt="HISTORIA CLINICA" className="img-clinical-history" />
+      <div className={styles.div2}>
+        <Link to={ROUTES.CLINICALHISTORY} className={styles['btn-clinical-history']}>
+          <img src={CLINICALHISTORY} alt="HISTORIA CLINICA" className={styles['img-clinical-history']} />
         </Link>
       </div>
 
-      <div className="div3">
-        <Link to={ROUTES.MONITORING} className="btn-monitoring">
-          <img src={MONITORING} alt="SEGUIMIENTO" className="img-monitoring" />
+      <div className={styles.div3}>
+        <Link to={ROUTES.MONITORING} className={styles['btn-monitoring']}>
+          <img src={MONITORING} alt="SEGUIMIENTO" className={styles['img-monitoring']} />
         </Link>
       </div>
 
-      <div className="div4">
-        <Link to={ROUTES.INFORMS} className="btn-informs">
-          <img src={INFORMS} alt="INFORMES SEGUIMIENTO" className="img-informs" />
+      <div className={styles.div4}>
+        <Link to={ROUTES.INFORMS} className={styles['btn-informs']}>
+          <img src={INFORMS} alt="INFORMES SEGUIMIENTO" className={styles['img-informs']} />
         </Link>
       </div>
 
-      <div className="div5">
-        <Link to={ROUTES.BASIC_DATA_EDITING} className="btn-editing">
-          <img src={EDITION} alt="EDICIÓN DE DATOS BÁSICOS" className="img-editing" />
+      <div className={styles.div5}>
+        <Link to={ROUTES.BASIC_DATA_EDITING} className={styles['btn-editing']}>
+          <img src={EDITION} alt="EDICIÓN DE DATOS BÁSICOS" className={styles['img-editing']} />
         </Link>
       </div>
 
-      <div className="div6">
-        <Link to={ROUTES.SETTINGS} className="btn-settings">
-          <img src={SETTINGS} alt="AJUSTES" className="img-settings" />
+      <div className={styles.div6}>
+        <Link to={ROUTES.SETTINGS} className={styles['btn-settings']}>
+          <img src={SETTINGS} alt="AJUSTES" className={styles['img-settings']} />
         </Link>
       </div>
     </div>
