1c9278c83b111002933f7112f1f80f1e2d7afe27
Alberto Mario Guerrero G <psico.amgg@gmail.com>
2024-01-12 20:39:54 -0500

    Segmentation

---
+++ client/src/components/Segmentation/Segmentation.jsx
@@ -0,0 +1,14 @@
+import styles from "./Segmentation.module.css";
+
+
+const Segmentation = () => {
+  return (
+    <>
+      <div className={styles.containerSegmentation}>
+         👋🏽👋🏽👋🏽 Hola, soy el componente llamado Segmentation
+      </div>
+    </>
+  );
+};
+
+export default Segmentation;
