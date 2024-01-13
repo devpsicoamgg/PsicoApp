8556344064107435579947894c70d19af261cfa4
Alberto Mario Guerrero G <psico.amgg@gmail.com>
2024-01-12 23:01:55 -0500

    Various

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './components/homePage/homePage';
-import BasicDataForm from './components/basicDataForm/BasicDataForm';
-import ClinicalHistory from './components/clinicalHistory/ClinicalHistory';
-import Monitoring from './components/monitoring/Monitoring';
-import Informs from './components/informs/Informs';
-import EditAndDeleteDataForm from './components/editAndDelete/EditAndDelete';
-import Settings from './components/settings/Settings';
-import ROUTES from './routes/routesHelper';
-import LogingForm from './components/loging/logingForm';
-import Segmentation from './components/Segmentation/Segmentation';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/homePage/homePage";
+import BasicDataForm from "./components/basicDataForm/BasicDataForm";
+import ClinicalHistory from "./components/clinicalHistory/ClinicalHistory";
+import Monitoring from "./components/monitoring/Monitoring";
+import Informs from "./components/informs/Informs";
+import EditAndDeleteDataForm from "./components/editAndDelete/EditAndDelete";
+import Settings from "./components/settings/Settings";
+import ROUTES from "./routes/routesHelper";
+import LogingForm from "./components/loging/logingForm";
+import Segmentation from "./components/Segmentation/Segmentation";
+import Various from "./components/Various/Various";
 
 function App() {
   return (
@@ -21,12 +22,11 @@ function App() {
         <Route path={ROUTES.CLINICALHISTORY} element={<ClinicalHistory />} />
         <Route path={ROUTES.MONITORING} element={<Monitoring />} />
         <Route path={ROUTES.INFORMS} element={<Informs />} />
-        <Route path={ROUTES.BASIC_DATA_EDITING} element={<EditAndDeleteDataForm />} />
+        <Route path={ROUTES.BASIC_DATA_EDITING} element={<EditAndDeleteDataForm />}/>
         <Route path={ROUTES.SETTINGS} element={<Settings />} />
         <Route path={ROUTES.SEGMENTATION} element={<Segmentation />} />
+        <Route path={ROUTES.VARIOUS} element={<Various />} />
       </Routes>
-
-      
     </>
   );
 }
--- client/src/components/homePage/homePage.jsx
+++ client/src/components/homePage/homePage.jsx
@@ -1,140 +1,19 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
-import ROUTES from "../../routes/routesHelper";
-import APERTURA from "../../assets/img/creacionUser.png";
-import HCAPERTURA from "../../assets/img/hcApetura.png";
-import CLINICALHISTORY from "../../assets/img/apertura.png";
-import MONITORING from "../../assets/img/seguimientos.png";
-import TITLETRACKING from "../../assets/img/titleTracking.png";
-import INFORMS from "../../assets/img/informes.png";
-import TITLEINFORMS from "../../assets/img/titleInforms.png";
-import EDITION from "../../assets/img/edicion.png";
-import TITLEEDITION from "../../assets/img/editandinact.png";
-import SETTINGS from "../../assets/img/ajustes.png";
-import WSP from "../../assets/img/wsp.png";
-import TITLEWSP from "../../assets/img/segment.png";
 import BACKGROUND_IMG from "../../assets/img/background.png";
-import OTHERSHERRA from "../../assets/img/background.png";
-import TITLEIMG from "../../assets/img/creacionPcteTitle.png";
+
 import styles from "./homePage.module.css";
+import Navbar from "../Navbar/Navbar";
 
 function HomePage() {
+  
   return (
-    <div
-      className={styles.parent}
-      style={{ backgroundImage: `url(${BACKGROUND_IMG})` }}
-    >
-      <div className={styles.div1}>
-        <NavLink to={ROUTES.BASIC_DATA_FORM}>
-          <img
-            src={APERTURA}
-            alt="BATOS BÁSICOS"
-            className={styles["img-create-user"]}
-          />
-          <img
-            src={TITLEIMG}
-            alt="BATOS BÁSICOS"
-            className={styles["title-create-user"]}
-          />
-        </NavLink>
-      </div>
-
-      <div className={styles.div2}>
-        <Link to={ROUTES.CLINICALHISTORY}>
-          <img
-            src={CLINICALHISTORY}
-            alt="HISTORIA CLINICA"
-            className={styles["img-clinical-history"]}
-          />
-          <img
-            src={HCAPERTURA}
-            alt="APERTURA"
-            className={styles["title-clinical-history"]}
-          />
-        </Link>
-      </div>
-
-      <div className={styles.div3}>
-        <Link to={ROUTES.MONITORING}>
-          <img
-            src={MONITORING}
-            alt="INFORMES SEGUIMIENTO"
-            className={styles["img-trackings"]}
-          />
-          <img
-            src={TITLETRACKING}
-            alt="Seguimiento"
-            className={styles["title-trackings"]}
-          />
-        </Link>
-      </div>
-
-      <div className={styles.div4}>
-        <Link to={ROUTES.INFORMS}>
-          <img src={INFORMS} alt="INFORMES" className={styles["img-informs"]} />
-          <img
-            src={TITLEINFORMS}
-            alt="Seguimiento"
-            className={styles["title-informs"]}
-          />
-        </Link>
-      </div>
-
-      <div className={styles.div5}>
-        <Link to={ROUTES.BASIC_DATA_EDITING}>
-          <img
-            src={EDITION}
-            alt="EDICIÓN DE DATOS BÁSICOS"
-            className={styles["img-editing"]}
-          />
-          <img
-            src={TITLEEDITION}
-            alt="EDICIÓN DE DATOS BÁSICOS"
-            className={styles["title-editing"]}
-          />
-        </Link>
-      </div>
-
-      <div className={styles.div6}>
-        <Link to={ROUTES.SEGMENTATION}>
-          <img
-            src={WSP}
-            alt="Mensajería por wsp"
-            className={styles["img-wsp"]}
-          />
-          <img
-            src={TITLEWSP}
-            alt="Mensajería por wsp"
-            className={styles["title-wsp"]}
-          />
-        </Link>
-      </div>
-
-      <div className={styles.div6}>
-        <Link to={ROUTES.BASIC_DATA_EDITING}>
-          <img
-
-            alt="Mensajería por wsp"
-            className={styles["img-wsp"]}
-          />
-          <img
-
-            alt="Mensajería por wsp"
-            className={styles["title-wsp"]}
-          />
-        </Link>
-      </div>
-
-      <div className={styles.div8}>
-        <Link to={ROUTES.SETTINGS}>
-          <img
-            src={SETTINGS}
-            alt="AJUSTES"
-            className={styles["img-settings"]}
-          />
-        </Link>
-      </div>
+    <>
+    <div 
+    className={styles.parent} 
+    style={{ backgroundImage: `url(${BACKGROUND_IMG})`, backgroundPosition: 'center' }}>
+      <Navbar />
     </div>
+    </>
   );
 }
 
