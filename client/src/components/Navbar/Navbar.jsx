ba1983658704f8d0d9e73174da7937f24b8d793b
Alberto Mario Guerrero G <psico.amgg@gmail.com>
2024-01-16 19:36:58 -0500

    more

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,142 +1,105 @@
 import { NavLink, Link } from "react-router-dom";
 import ROUTES from "../../routes/routesHelper";
 import APERTURA from "../../assets/img/creacionUser.png";
-import HCAPERTURA from "../../assets/img/hcApetura.png";
 import CLINICALHISTORY from "../../assets/img/apertura.png";
 import MONITORING from "../../assets/img/seguimientos.png";
-import TITLETRACKING from "../../assets/img/titleTracking.png";
+import TRAKINGIMG from "../../assets/img/hcApetura.png";
 import INFORMS from "../../assets/img/informes.png";
-import TITLEINFORMS from "../../assets/img/titleInforms.png";
 import EDITION from "../../assets/img/edicion.png";
-import TITLEEDITION from "../../assets/img/editandinact.png";
 import SETTINGS from "../../assets/img/ajustes.png";
 import WSP from "../../assets/img/wsp.png";
-import TITLEWSP from "../../assets/img/segment.png";
-import AJUSTES from "../../assets/img/ajsutes.png";
-import TITLEIMG from "../../assets/img/creacionPcteTitle.png";
 import VARIOUS from "../../assets/img/various.png";
+
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <>
       <div className={styles.containerNavbar}>
-        <span>
-          <NavLink to={ROUTES.BASIC_DATA_FORM}>
+        <p>
+          <Link to={ROUTES.BASIC_DATA_FORM}>
             <img
               src={APERTURA}
               alt="BATOS BÁSICOS"
               className={styles["img-create-user"]}
             />
-            <img
-              src={TITLEIMG}
-              alt="BATOS BÁSICOS"
-              className={styles["title-create-user"]}
-            />
-          </NavLink>
-        </span>
+          </Link>
+        </p>
 
-        <span>
+        <p>
           <Link to={ROUTES.CLINICALHISTORY}>
             <img
               src={CLINICALHISTORY}
               alt="HISTORIA CLINICA"
               className={styles["img-clinical-history"]}
             />
-            <img
-              src={HCAPERTURA}
-              alt="APERTURA"
-              className={styles["title-clinical-history"]}
-            />
           </Link>
-        </span>
+        </p>
 
-        <span>
+        <p>
           <Link to={ROUTES.MONITORING}>
             <img
               src={MONITORING}
               alt="INFORMES SEGUIMIENTO"
               className={styles["img-trackings"]}
             />
-            <img
-              src={TITLETRACKING}
-              alt="Seguimiento"
-              className={styles["title-trackings"]}
-            />
+          <img src={TRAKINGIMG} alt="Seguimiento" className={styles.title} />
           </Link>
-        </span>
+        </p>
 
-        <span>
+        <p>
           <Link to={ROUTES.INFORMS}>
             <img
               src={INFORMS}
               alt="INFORMES"
               className={styles["img-informs"]}
             />
-            <img
-              src={TITLEINFORMS}
-              alt="Seguimiento"
-              className={styles["title-informs"]}
-            />
           </Link>
-        </span>
+        </p>
 
-        <span>
-          <Link to={ROUTES.BASIC_DATA_EDITING}>
-            <img
-              src={EDITION}
-              alt="EDICIÓN DE DATOS BÁSICOS"
-              className={styles["img-editing"]}
-            />
-            <img
-              src={TITLEEDITION}
-              alt="EDICIÓN DE DATOS BÁSICOS"
-              className={styles["title-editing"]}
-            />
-          </Link>
-        </span>
-
-        <span>
-          <Link to={ROUTES.SEGMENTATION}>
-            <img
-              src={WSP}
-              alt="Mensajería por wsp"
-              className={styles["img-wsp"]}
-            />
-            <img
-              src={TITLEWSP}
-              alt="Mensajería por wsp"
-              className={styles["title-wsp"]}
-            />
-          </Link>
-        </span>
-
-        <span>
-          <Link to={ROUTES.VARIOUS}>
-            <img
-              src={VARIOUS}
-              alt="Varios 7"
-              className={styles["img-various"]}
-            />
-            <img className={styles["title-various"]} />
-          </Link>
-        </span>
-
-        <span>
-          <Link to={ROUTES.SETTINGS}>
-            <img
-              src={SETTINGS}
-              alt="AJUSTES"
-              className={styles["img-settings"]}
-            />
-            <img
-              src={AJUSTES}
-              alt="Mensajería por wsp"
-              className={styles["title-settings"]}
-            />
-          </Link>
-        </span>
       </div>
+<div>
+<p>
+        <Link to={ROUTES.SETTINGS}>
+          <img
+            src={SETTINGS}
+            alt="AJUSTES"
+            className={styles["img-settings"]}
+          />
+        </Link>
+        </p>
+
+        <p>
+        <Link to={ROUTES.BASIC_DATA_EDITING}>
+          <img
+            src={EDITION}
+            alt="EDICIÓN DE DATOS BÁSICOS"
+            className={styles["img-editing"]}
+          />
+        </Link>
+        </p>
+        
+
+<p> 
+        <Link to={ROUTES.SEGMENTATION}>
+          <img
+            src={WSP}
+            alt="Mensajería por wsp"
+            className={styles["img-wsp"]}
+          />
+        </Link>
+        </p>
+
+<p> 
+        <Link to={ROUTES.VARIOUS}>
+          <img src={VARIOUS} alt="Varios 7" className={styles["img-various"]} />
+        </Link>
+        </p>
+
+      
+
+ </div>
+
     </>
   );
 };
--- client/src/components/loging/LogingForm.jsx
+++ client/src/components/loging/LogingForm.jsx
@@ -5,7 +5,18 @@ const LogingForm = () => {
   return (
     <>
       <div className={styles.containerLogingForm}>
-         👋🏽👋🏽👋🏽 Hola, soy el componente llamado LogingForm
+       
+       <div className={styles.inputGroup}>
+      <input className={styles.input} required type={"text"} id={"fistName"} ></input>
+      <label className={styles.label} htmlFor={"fistName"}> Primer nombre </label>
+       </div>
+
+       <div className={styles.inputGroup}>
+      <input className={styles.input} required type={"text"} id={"secondName"} ></input>
+      <label className={styles.label} htmlFor={"secondName"}> Segundo nombre </label>
+       </div>
+
+
       </div>
     </>
   );
