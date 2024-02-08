12152531e652961f44c14e6c28adcca50704a896
devpsicoamgg <psico.amgg@gmail.com>
2024-02-08 17:55:10 -0500

    Logging

--- client/src/components/loging/LogingForm.jsx
+++ client/src/components/loging/LogingForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./LogingForm.module.css";
 import ROUTES from "../../routes/routesHelper";
+import imagen from "../../assets/img/background.png";
 
 const LogingForm = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -15,28 +16,39 @@ const LogingForm = () => {
   return (
     <>
       <div className={styles.containerLogingForm}>
-        <div className={styles.inputGroup}>
-          <input className={styles.input} required type="text" id="firstName" />
-          <label className={styles.label} htmlFor="firstName">
-            Primer nombre
-          </label>
+        <div className={styles.imageContainer}>
+          <img src={imagen} alt="Imagen" className={styles.image} />
         </div>
 
-        <div className={styles.inputGroup}>
-          <input
-            className={styles.input}
-            required
-            type="text"
-            id="secondName"
-          />
-          <label className={styles.label} htmlFor="secondName">
-            Segundo nombre
-          </label>
-        </div>
+        <div className={styles.formContainer}>
+          <div className={styles.inputGroup}>
+            <input
+              className={styles.input}
+              required
+              type="text"
+              id="firstName"
+            />
+            <label className={styles.label} htmlFor="firstName">
+              Primer nombre
+            </label>
+          </div>
 
-        <button className={styles.button} onClick={handleLogin}>
-          Iniciar sesión
-        </button>
+          <div className={styles.inputGroup}>
+            <input
+              className={styles.input}
+              required
+              type="text"
+              id="secondName"
+            />
+            <label className={styles.label} htmlFor="secondName">
+              Segundo nombre
+            </label>
+          </div>
+
+          <button className={styles.button} onClick={handleLogin}>
+            Iniciar sesión
+          </button>
+        </div>
       </div>
     </>
   );
