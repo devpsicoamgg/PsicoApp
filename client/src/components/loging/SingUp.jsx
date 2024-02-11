00c7eac98832d8db2483838c5fb3064df74f980d
devpsicoamgg <psico.amgg@gmail.com>
2024-02-11 01:41:35 -0500

    More

---
+++ client/src/components/loging/SingUp.jsx
@@ -0,0 +1,14 @@
+
+
+
+const SingUp = () => {
+  return (
+    <>
+      <div className={styles.containerSingUp}>
+         👋🏽👋🏽👋🏽 Hola, soy el componente llamado SingUp
+      </div>
+    </>
+  );
+};
+
+export default SingUp;
